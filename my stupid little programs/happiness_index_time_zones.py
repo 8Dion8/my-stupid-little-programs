ca4aba1d6f1a6eb955bb7c6ba3ca758813b0db68
8Dion8 <shvartserinfo@gmail.com>
2020-11-04 22:00:21 +0300

    lul

--- my stupid little programs/happiness_index_time_zones.py
+++ my stupid little programs/happiness_index_time_zones.py
@@ -2,7 +2,7 @@ import requests as r
 from bs4 import BeautifulSoup
 import openpyxl as o
 import matplotlib.pyplot as plt
-
+'''
 url = 'https://timezonedb.com/time-zones'
 timezones = {}
 final = {}
@@ -19,22 +19,19 @@ for i in rows:
         parts = timezone.split(':')
     except:pass
     #print(timezone)
-    timezones[collumns[1].text] = eval("".join(parts[0].split('0')) + '.' + str(int(parts[1])//6))
+    if collumns[1].text in timezones.keys():
+        timezones[collumns[1].text].append(eval("".join(parts[0].split('0')) + '.' + str(int(parts[1])//6)))
+    else:
+        timezones[collumns[1].text] = [eval("".join(parts[0].split('0')) + '.' + str(int(parts[1])//6))]
+
 
 print(timezones)
-
+'''
 wb = o.load_workbook('WHR20_DataForFigure2.1.xlsx')
 
 sheet = wb.get_sheet_by_name('Sheet1')
 
-for row in sheet['A1':'C154']:
-    country = row[0].value
-    try:
-        timezone = timezones[country]
-        if country in final.keys():
-            final[country].append(row[2].value)
-        else:
-            final[country] = [row[2].value]
-    except:print(country)
+happiness = sheet['A1':'C154']
+print(happiness)
 
-print(final)
+#print(final)
